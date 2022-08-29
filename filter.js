e90425cb4269eabe7fb6c96ea11845077d515563
Awfulot <Mr.Snisken1@gmail.com>
2022-08-29 14:49:56 -0400

    Created basic button mockup, added all relevant JS variables

---
+++ filter.js
@@ -0,0 +1,117 @@
+const lakefrontButton = document.getElementById("lakefront-button");
+const beachButton = document.getElementById("beach-button");
+const cabinButton = document.getElementById("cabin-button");
+const omgButton = document.getElementById("omg-button");
+const nationalButton = document.getElementById("national-button");
+const tinyButton = document.getElementById("tiny-button");
+
+const displayHomesDiv = document.getElementById("display-homes");
+
+const homes = [
+    {
+        type: 'Lakefront',
+        image: 'https://picsum.photo/200?random=1',
+        address: '1 Sample St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'Beach',
+        image: 'https://picsum.photo/200?random=1',
+        address: '1 James St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'Beach',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Anon St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'National parks',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Sample St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'National parks',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Sample St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'National parks',
+        image: 'https://picsum.photo/200?random=1',
+        address: '1 Sample St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'OMG',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Jerry St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'OMG',
+        image: 'https://picsum.photo/200?random=1',
+        address: '1 Josh St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'Tiny homes',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Berry St',
+        city: 'Boston',
+        rent: 200
+    },
+    {
+        type: 'Cabins',
+        image: 'https://picsum.photo/200?random=1',
+
+        address: '1 Jerry St',
+        city: 'Boston',
+        rent: 200
+    },
+];
+
+
+
+function buttonManager(){
+
+};
+
+lakefrontButton.addEventListener("click", function (){
+
+});
+
+beachButton.addEventListener("click", function (){
+
+});
+
+cabinButton.addEventListener("click", function (){
+
+});
+
+omgButton.addEventListener("click", function (){
+
+});
+
+nationalButton.addEventListener("click", function (){
+
+});
+
+tinyButton.addEventListener("click", function (){
+
+});
